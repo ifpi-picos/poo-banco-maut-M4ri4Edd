3eb08baa42492c6cf9ab4f7800c038eabc026420
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-10-24 17:19:00 -0300

    métodos da poupança

--- BankMaut-2/src/ContaPoupanca.java
+++ BankMaut-2/src/ContaPoupanca.java
@@ -1,5 +1,42 @@
 public class ContaPoupanca extends Conta {
-    public ContaPoupanca(String number, String agencyNumber, String client, double balance, Transacao transacoes) {
+    public ContaPoupanca(String number, String agencyNumber, Client client, double balance, Transacao transacoes) {
         super(number, agencyNumber, client, balance, transacoes);
     }
+
+    @Override
+    public void depositar(double deposit) {
+        if (deposit > 0) {
+            double rendimentoDeposito = deposit + (deposit * 0.1);
+            super.balance += rendimentoDeposito;
+            getNotificacao().enviarNotificacao(deposit, "Depósito");
+            getTransacoes().add(new Transacao(deposit, "Depósito"));
+        }
+    }
+
+    @Override
+    public void Sacar(double saque){
+        double valorTaxado = saque + (saque * 0.05);
+        if (valorTaxado <= balance){
+            setBalance(balance -= valorTaxado);
+            System.out.println("Saque realizado com sucesso");
+            getNotificacao().enviarNotificacao(saque, "Saque da Conta Poupança");
+            getTransacoes().add(new Transacao(saque, "Saque da Conta Poupança"));
+        } else {
+            System.out.println("Saldo Insuficiente.");
+        }
+    }
+
+    @Override
+    public void Transferir(double transferencia){
+        double valorTaxado = transferencia + (transferencia * 0.05);
+        if (transferencia <= valorTaxado){
+            setBalance(balance -= valorTaxado);;
+            System.out.println("Transferência realizada com sucesso!");
+            getNotificacao().enviarNotificacao(transferencia, "Transferência da Conta Poupança");
+            getTransacoes().add(new Transacao(transferencia, "Transferência da Conta Poupança"));
+        } else {
+            System.out.println("Tranferência Interrompida: Saldo Insuficiente.");
+        }
+    }
+
 }
