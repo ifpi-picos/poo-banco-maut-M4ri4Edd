9c4e638d6f184d3e2f01b52f7837928d784a50d0
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-10-24 19:42:28 -0300

    último ajuste

--- BankMout/src/Conta.java
+++ BankMout/src/Conta.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Conta {
-    public static final String getBalance = null;
     private String number;
     private String agencyNumber;
     private String client;
@@ -45,6 +44,7 @@ public class Conta {
         if (deposit > 0){
             setBalance(balance += deposit);
             System.out.println("Depósito realizado com sucesso");
+            getTransacoes().add(new Transacao(deposit, "Depósito"));
         } else{
             System.out.println("Depósito Inválido");
         }
@@ -54,6 +54,7 @@ public class Conta {
         if (saque <= balance){
             setBalance(balance -= saque);
             System.out.println("Saque realizado com sucesso");
+            getTransacoes().add(new Transacao(saque, "Saque"));
         } else {
             System.out.println("Saldo Insuficiente.");
         }
@@ -63,15 +64,12 @@ public class Conta {
         if (transferencia <= balance){
             setBalance(balance -= transferencia);;
             System.out.println("Transferência realizada com sucesso!");
+            getTransacoes().add(new Transacao(transferencia, "Transferência"));
         } else {
             System.out.println("Tranferência Interrompida: Saldo Insuficiente.");
         }
     }
 
-    public void adicionarTransacao(double value, String type){
-        Transacao novaTransacao = new Transacao(value, type);
-        transacoes.add(novaTransacao);
-    }
     public void verExtrato(){
         for(Transacao novaTransacao : transacoes){
             System.out.println(novaTransacao.toString());
