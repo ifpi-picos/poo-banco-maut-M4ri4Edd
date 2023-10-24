a5ccfbeeced7be089f97b38ff6525dd43c79e830
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-10-23 22:09:53 -0300

    Ajustes no app

--- BankMout/src/App.java
+++ BankMout/src/App.java
@@ -1,20 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        Client client1 = new Client("Maria Luz", "000.000.000-00", "18/10/2005", new Endereço(791, "ACL", "Ipueiras", "Picos", "PI", "64000"));
+        List<Conta> contas = new ArrayList<>();
+        Client client1 = new Client("Maria Luz", "000.000.000-00", "18/10/2005", new Endereço("791", "ACL", "Ipueiras", "Picos", "PI", "64000"));
         Scanner scanner = new Scanner(System.in);
+
+        contas.add(new Conta("1", "00001", "client1", 100, null));
+        contas.add(new Conta("2", "00001", "client1", 100, null));
 
         while(true){
             System.out.println("MENU - BANCO MAUT");
-            System.out.println("1) Cadastrar conta");
-            System.out.println("2) Depositar");
-            System.out.println("3) Sacar");
-            System.out.println("4) Transferir");
-            System.out.println("5) Consultar Informações da conta");
-            System.out.println("6) Consultar Extrato");
-            System.out.println("7) Sair");
+            System.out.println("1) Depositar");
+            System.out.println("2) Sacar");
+            System.out.println("3) Transferir");
+            System.out.println("4) Consultar Informações da conta");
+            System.out.println("5) Consultar Extrato");
+            System.out.println("6) Sair");
             System.out.println("Selecione a opção desejada");
 
             int option = scanner.nextInt();
@@ -25,23 +29,15 @@ public class App {
                 String number = scanner.nextLine();
                 System.out.println("Digite o número da agência");
                 String agencyNumber = scanner.nextLine();
-                System.out.println("Digite o valor de abertura");
-                double abertura = scanner.nextDouble();
-                scanner.nextLine();
-                Conta conta = new Conta(number, agencyNumber, abertura, client1);
-                System.out.println("Conta criada com sucesso");
-            } else if(option == 2){
-                System.out.println("Digite o número da conta");
-                String number = scanner.nextLine();
-                System.out.println("Digite o número da agência");
-                String agencyNumber = scanner.nextLine();
                 System.out.println("Digite o valor do depósito");
                 double deposit = scanner.nextDouble();
                 scanner.nextLine();
-                Conta conta = new Conta(number, agencyNumber, 0, null);
-                conta.depositar(deposit);
-                System.out.println("Depósito realizado com sucesso");
-            } else if(option == 3){
+                for(Conta conta : contas){
+                    if(conta.getNumber() == number && conta.getAgencyNumber() == agencyNumber){
+                        conta.depositar(deposit);
+                    }
+                }
+            } else if(option == 2){
                 System.out.println("Digite o número da conta");
                 String number = scanner.nextLine();
                 System.out.println("Digite o número da agência");
@@ -49,28 +45,55 @@ public class App {
                 System.out.println("Digite o valor do Saque");
                 double saque = scanner.nextDouble();
                 scanner.nextLine();
-                Conta conta = new Conta(number, agencyNumber, 0, null);
-                conta.Sacar(saque);
+                for(Conta conta : contas){
+                    if(conta.getNumber() == number && conta.getAgencyNumber() == agencyNumber){
+                        conta.Sacar(saque);
+                    }
+                }
+            } else if(option == 3){
+                System.out.println("Digite o número da conta de origem");
+                String numberOrigem = scanner.nextLine();
+                System.out.println("Digite o número da agência de origem");
+                String agencyNumberOrigem = scanner.nextLine();
+                System.out.println("Digite o número da conta de destino");
+                String numberDestino = scanner.nextLine();
+                System.out.println("Digite o número da agência de destino");
+                String agencyNumberDestino = scanner.nextLine();
+                System.out.println("Digite o valor da transferência");
+                double transferencia = scanner.nextDouble();
+                scanner.nextLine();
+                for(Conta conta : contas){
+                    if(conta.getNumber() == numberOrigem && conta.getAgencyNumber() == agencyNumberOrigem){
+                        conta.Transferir(transferencia);
+                    }
+                }
             } else if(option == 4){
-                    System.out.println("Digite o número da conta de origem");
-                    String numberOrigem = scanner.nextLine();
-                    System.out.println("Digite o número da agência de origem");
-                    String agencyNumberOrigem = scanner.nextLine();
-                    System.out.println("Digite o número da conta de destino");
-                    String numberDestino = scanner.nextLine();
-                    System.out.println("Digite o número da agência de destino");
-                    String agencyNumberDestino = scanner.nextLine();
-                    System.out.println("Digite o valor da transferência");
-                    double transferencia = scanner.nextDouble();
-                    scanner.nextLine();
-                    Conta contaOrigem = new Conta(numberOrigem, agencyNumberOrigem, 0, null);
-                    Conta contaDestino = new Conta(numberDestino, agencyNumberDestino, 0, null);
-                    contaOrigem.Transferir(contaDestino, transferencia);
+                System.out.println("Digite o Numero da conta que deseja vizualizar: ");
+                String number = scanner.nextLine();
+                System.out.println("Digite o Numero da agência da conta: ");
+                String agencyNumber = scanner.nextLine();
+                for(Conta conta : contas){
+                    if(conta.getNumber() == number && conta.getAgencyNumber() == agencyNumber){
+                        System.out.println("INFORMAÇÕES DA CONTA " + number);
+                        System.out.println("Cliente: " + client1.getName());
+                        System.out.println("Número da conta: " + number);
+                        System.out.println("Agência: " + agencyNumber);
+                        System.out.println("CPF: " + client1.getCPF());
+                        System.out.println("Data de Nascimento :" + client1.getBirthdate());
+                        System.out.println("HISTÓRICO DE TRANSAÇÕES");
+                    }
+                }
             } else if(option == 5){
-                showInfo();
+                System.out.println("Digite o Numero da conta que deseja vizualizar: ");
+                String number = scanner.nextLine();
+                System.out.println("Digite o Numero da agência da conta: ");
+                String agencyNumber = scanner.nextLine();
+                for(Conta conta : contas){
+                    if(conta.getNumber() == number && conta.getAgencyNumber() == agencyNumber){
+                        System.out.println("Saldo da conta " + number + ": R$ " + conta.getBalance);
+                    }
+                }
             } else if(option == 6){
-                showBalance();
-            } else if(option == 7){
                 System.out.print("Você saiu do programa!");
                 break;
             } else {
