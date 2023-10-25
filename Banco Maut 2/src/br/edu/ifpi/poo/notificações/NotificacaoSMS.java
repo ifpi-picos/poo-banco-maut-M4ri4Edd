bc710d7fc96ac9008020fe499486e0a115bfb9f7
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-10-24 22:57:53 -0300

    Update NotificacaoSMS.java

--- Banco Maut 2/src/br/edu/ifpi/poo/notificações/NotificacaoSMS.java
+++ Banco Maut 2/src/br/edu/ifpi/poo/notificações/NotificacaoSMS.java
@@ -4,8 +4,10 @@ public class NotificacaoSMS implements Notificacao {
 
     @Override
     public void enviarNotificacao(double value, String type) {
+        System.out.println("___________________________________");
         System.out.println("Enviando notificação por SMS");
-        System.out.println("\nTipo: " + type);
+        System.out.println("Tipo: " + type);
         System.out.println("Valor: " + value);
+        System.out.println("___________________________________");
     }
 }
