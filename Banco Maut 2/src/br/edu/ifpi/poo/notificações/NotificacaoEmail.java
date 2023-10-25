062294739005763dfb7b775e7e01f113187cc124
M4ri4Edd <135741080+M4ri4Edd@users.noreply.github.com>
2023-10-24 22:57:27 -0300

    Update NotificacaoEmail.java

--- Banco Maut 2/src/br/edu/ifpi/poo/notificações/NotificacaoEmail.java
+++ Banco Maut 2/src/br/edu/ifpi/poo/notificações/NotificacaoEmail.java
@@ -4,8 +4,10 @@ public class NotificacaoEmail implements Notificacao {
 
     @Override
     public void enviarNotificacao(double value, String type) {
-        System.out.println("Enviando notificação por e-mail");
-        System.out.println("\nTipo: " + type);
+        System.out.println("___________________________________");
+        System.out.println("Enviando notificação por E-mail");
+        System.out.println("Tipo: " + type);
         System.out.println("Valor: " + value);
+        System.out.println("___________________________________");
     }
 }
